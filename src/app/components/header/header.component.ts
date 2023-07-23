1ad0fdfceae327e294e94ac87349e28ee516d289
Lorenzo <lmiscoli@gmail.com>
2023-07-23 16:03:41 +0200

    Create sidenav in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { ClickOutsideDirective } from "src/app/directives/click-outside.directive";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
-  standalone: true
+  standalone: true,
+  imports: [ClickOutsideDirective]
 })
 export class HeaderComponent {
+  @ViewChild("sidenav") sidenav!: ElementRef;
+
+  public openSidenav(): void {
+    this.sidenav.nativeElement.classList.add("show");
+  }
+
+  public closeSidenav(): void {
+    this.sidenav.nativeElement.classList.remove("show");
+  }
 
 }
---
+++ src/app/directives/click-outside.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, EventEmitter, HostListener, Output, Input } from '@angular/core';
+
+@Directive({
+  selector: '[clickOutside]',
+  standalone: true
+})
+export class ClickOutsideDirective {
+  @Output() clickOutside = new EventEmitter<MouseEvent>();
+  @Input() skipClass!: string;
+  constructor(private elementRef: ElementRef) { }
+
+  @HostListener('document:click', ['$event', '$event.target'])
+  public onClick(event: MouseEvent, targetElement: HTMLElement): void {
+    if (!targetElement) {
+      return;
+    }
+    if (targetElement.classList.contains(this.skipClass)) {
+      return;
+    }
+    const clickedInside = this.elementRef.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.clickOutside.emit(event);
+    }
+  }
+}
