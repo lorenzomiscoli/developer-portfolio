6fa8bdd3b60ad93730b28e8e4a56319558111f32
Lorenzo <lmiscoli@gmail.com>
2023-07-27 18:42:32 +0200

    Create landing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './layouts/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
