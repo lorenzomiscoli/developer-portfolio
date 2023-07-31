402403a1ef243853e68a3a74a92ad1b482de86b7
Lorenzo <lmiscoli@gmail.com>
2023-07-31 08:28:16 +0200

    Create about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './layouts/landing/landing.component';
+import { AboutComponent } from './layouts/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LandingComponent],
+  imports: [CommonModule, HeaderComponent, LandingComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
