324b7ca9775434c7f9b86b82811e897f9ab33e9b
Lorenzo <lmiscoli@gmail.com>
2023-08-02 10:14:04 +0200

    Create services component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './layouts/landing/landing.component';
 import { AboutComponent } from './layouts/about/about.component';
+import { ServicesComponent } from './layouts/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LandingComponent, AboutComponent],
+  imports: [CommonModule, HeaderComponent, LandingComponent, AboutComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/layouts/services/services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-services",
+  templateUrl: "./services.component.html",
+  styleUrls: ["./services.component.scss"],
+  standalone: true
+})
+export class ServicesComponent {
+
+}
