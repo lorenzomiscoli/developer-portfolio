664b636e549827aef6278781ab00d5428399ed37
Lorenzo <lmiscoli@gmail.com>
2023-08-05 07:56:48 +0200

    Add sticky header

--- src/app/components/header/header.dependencies.ts
+++ src/app/components/header/header.dependencies.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { SidenavComponent } from "../sidenav/sidenav.component";
 import { RouterModule } from "@angular/router";
+import { ScrollDirective } from "src/app/directives/scroll-directive";
 
-export const HEADER_DEPS = [CommonModule, SidenavComponent, RouterModule];
+export const HEADER_DEPS = [CommonModule, SidenavComponent, RouterModule, ScrollDirective];
