fc0226860f75b2ee9c1ab0a56ddec8db8283aa18
Lorenzo <lmiscoli@gmail.com>
2023-09-01 15:43:18 +0200

    Add NgOptimizedImage directive to images

--- src/app/components/header/header.dependencies.ts
+++ src/app/components/header/header.dependencies.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
 import { SidenavComponent } from "../sidenav/sidenav.component";
 import { RouterModule } from "@angular/router";
 import { ScrollDirective } from "src/app/directives/scroll-directive";
 
-export const HEADER_DEPS = [CommonModule, SidenavComponent, RouterModule, ScrollDirective];
+export const HEADER_DEPS = [CommonModule, NgOptimizedImage, SidenavComponent, RouterModule, ScrollDirective];
